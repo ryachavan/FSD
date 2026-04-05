b4e6251b33777f4e2021a435e9536e9f9d8e93fa
Arya <navahcayra@gmail.com>
2026-04-06 01:05:17 +0530

    exp 4b and 5b

---
+++ joke-app/server.js
@@ -0,0 +1,30 @@
+import express from "express";
+import axios from "axios";
+
+const app = express();
+const port = 3000;
+
+app.set("view engine", "ejs");
+
+app.get("/", async (req, res) => {
+  try {
+    const response = await axios.get(
+      "https://v2.jokeapi.dev/joke/Programming?safe-mode&type=single"
+    );
+
+    const jokeText = response.data.joke;
+
+    res.render("index", { joke: jokeText });
+
+  } catch (error) {
+    console.error(error.message);
+
+    res.render("index", {
+      joke: "Unable to fetch joke. Please try again."
+    });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
