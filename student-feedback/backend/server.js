591b1a148cf90b22ade881b5ae6bf74caf3d0721
Arya <navahcayra@gmail.com>
2026-04-16 09:27:05 +0530

    Revert "exp 6 deploy"
    This reverts commit b896f54cfc66c9a18b3b6d7457dc289502aeb84d.

--- student-feedback/backend/server.js
+++ student-feedback/backend/server.js
@@ -70,17 +70,5 @@ app.use((err, req, res, next) => {
 // Start Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
-
-import path from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-app.use(express.static(path.join(__dirname, "../dist")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../dist/index.html"));
+  console.log(`Server running on http://localhost:${PORT}`);
 });
